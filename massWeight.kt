722bcf28e27ec31a3b6178114728b3d27116b5d3
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-03-09 09:39:40 -0600

    Update massWeight.kt

--- massWeight.kt
+++ massWeight.kt
@@ -1,16 +1,12 @@
-import java.util.Scanner
-
 fun main() {
-    val input = Scanner(System.`in`)
+    print("Enter the mass of the object (in kg): ")
+    val mass = readLine()!!.toDouble()
 
-    print("Enter the mass of the object in kilograms: ")
-    val mass = input.nextDouble()
-
-    val weight = mass * 9.8
+    val weight = mass * 9.81 // 9.81 m/s^2 is the acceleration due to gravity on Earth
 
     when {
         weight > 1000 -> println("The object is too heavy.")
         weight < 10 -> println("The object is too light.")
-        else -> println("The weight of the object is %.2f Newtons.".format(weight))
+        else -> println("The weight of the object is $weight N.")
     }
 }
