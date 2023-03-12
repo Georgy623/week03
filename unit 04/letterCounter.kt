f35283f871627e806f49a386c7eb8fa577f1dbb2
Georgy623 <90360245+Georgy623@users.noreply.github.com>
2023-03-12 15:28:26 -0500

    Update letterCounter.kt

--- unit 04/letterCounter.kt
+++ unit 04/letterCounter.kt
@@ -1,13 +1,9 @@
-import java.util.Scanner
-
 fun main() {
-    val scanner = Scanner(System.`in`)
-
     print("Enter a string: ")
-    val str = scanner.nextLine()
+    val str = readLine()!!
 
     print("Enter a character: ")
-    val ch = scanner.next()[0]  
+    val ch = readLine()!![0]  
 
     val count = str.count { it == ch }
     println("The character '$ch' appears $count times in the string.")
